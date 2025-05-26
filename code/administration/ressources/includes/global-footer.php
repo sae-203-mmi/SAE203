61121a1dbdce80444ba19219ca9975901566bd04
sae-203-mmi <sae.203.mmi@gmail.com>
2025-05-26 23:00:59 +0200

    MAJ Halima

--- code/administration/ressources/includes/global-footer.php
+++ code/administration/ressources/includes/global-footer.php
@@ -8,8 +8,10 @@
     <p class="font-bold">MMI <?php echo (date("Y") - 1) . "-" . (date("Y") + 2); ?></p>
     <p>Projet réalisé par :</p>
     <ul class="inline-flex">
-        <li class="px-1">A compléter</li>
-        <li class="px-1">A compléter</li>
-        <li class="px-1">A compléter</li>
+        <li class="px-1">AIDARA Marianne </li>
+        <li class="px-1">BAHOU Calista </li>
+        <li class="px-1">BOUKHRIS Halima </li>
+        <li class="px-1">CAMARA Aliya </li>
+        <li class="px-1">CHELALI Bouchra </li>
     </ul>
 </footer>
