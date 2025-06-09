f267e8d4171b33277f5abe049dbcca7a86ef648d
AM-247 <marianne.aidara@yahoo.com>
2025-06-09 22:53:08 +0200

    Modification Page Administration et Page Article

--- code/administration/articles/index.php
+++ code/administration/articles/index.php
@@ -38,6 +38,7 @@ $URL_creation = "{$racine_URL}/creation.php";
         <div class="mx-auto max-w-7xl py-3 px-4 justify-between flex">
             <div>
                 <p class="text-3xl font-bold text-gray-900">Liste Articles</p>
+                <p class="text-gray-500 text-sm">Nombre d'articles : <?php echo mysqli_num_rows($resultat_brut); ?></p>
             </div>
             <a href="<?php echo $URL_creation ?>" class="self-start block rounded-md py-2 px-4 text-base font-medium text-white shadow-sm bg-slate-700 hover:bg-slate-900 focus-within:bg-slate-900">Ajouter un nouvel article</a>
         </div>
--- code/administration/auteurs/edition.php
+++ code/administration/auteurs/edition.php
@@ -3,6 +3,7 @@ require_once '../../ressources/includes/connexion-bdd.php';
 
 $page_courante = 'auteurs';
 
+$errors = [];
 $formulaire_soumis = !empty($_POST);
 $id_present_url = array_key_exists('id', $_GET);
 
@@ -14,32 +15,56 @@ if ($id_present_url) {
     $entite = mysqli_fetch_array($resultat_brut, MYSQLI_ASSOC);
 }
 
+$nom = $_POST['nom'] ?? '';
+$prenom = $_POST['prenom'] ?? '';
+$lien_avatar = $_POST['lien_avatar'] ?? '';
+$lien_twitter = $_POST['lien_twitter'] ?? '';
+
 if ($formulaire_soumis) {
-    $id = $_POST["id"];
-    $nom = htmlentities($_POST["nom"]);
-    $prenom = htmlentities($_POST["prenom"]);
-    $lien_avatar = htmlentities($_POST["lien_avatar"]);
-    $lien_twitter = htmlentities($_POST["lien_twitter"]);
+    // Validation des champs requis
+    if (empty(trim($nom))) {
+        $errors['nom'] = "Le nom est requis.";
+    }
+    if (empty(trim($prenom))) {
+        $errors['prenom'] = "Le prénom est requis.";
+    }
+    if (empty(trim($lien_avatar))) {
+        $errors['lien_avatar'] = "Le lien de l'avatar est requis.";
+    } elseif (!filter_var($lien_avatar, FILTER_VALIDATE_URL)) {
+        $errors['lien_avatar'] = "Le lien de l'avatar doit être une URL valide.";
+    }
+    if (!empty(trim($lien_twitter)) && !filter_var($lien_twitter, FILTER_VALIDATE_URL)) {
+        $errors['lien_twitter'] = "Le lien Twitter doit être une URL valide.";
+    }
+    // Si aucun champ n'a d'erreur, on procède à l'édition
+    if (empty($errors)) {
+        $id = $_POST["id"];
+        $nom = htmlentities($_POST["nom"]);
+        $prenom = htmlentities($_POST["prenom"]);
+        $lien_avatar = htmlentities($_POST["lien_avatar"]);
+        $lien_twitter = htmlentities($_POST["lien_twitter"]);
 
-    $requete_brute = "
-        UPDATE auteur
-        SET
-            nom = '$nom',
-            prenom = '$prenom',
-            lien_avatar ='$lien_avatar',
-            lien_twitter = '$lien_twitter'
-        WHERE id = '$id'
-    ";
+        $requete_brute = "
+            UPDATE auteur
+            SET
+                nom = '$nom',
+                prenom = '$prenom',
+                lien_avatar ='$lien_avatar',
+                lien_twitter = '$lien_twitter'
+            WHERE id = '$id'
+        ";
 
-    $resultat_brut = mysqli_query($mysqli_link, $requete_brute);
+        $resultat_brut = mysqli_query($mysqli_link, $requete_brute);
 
-    if ($resultat_brut === true) {
-        $racineURL = pathinfo($_SERVER['REQUEST_URI']);
-        $pageRedirection = $racineURL['dirname'];
-        header("Location: $pageRedirection");
-        // Tout s'est bien passé
-    } else {
-
+        if ($resultat_brut === true) {
+            $racineURL = pathinfo($_SERVER['REQUEST_URI']);
+            $pageRedirection = $racineURL['dirname'];
+            header("Location: $pageRedirection");
+            // Tout s'est bien passé, l'utilisateur retourne à la liste des auteurs
+        } else {
+            // Il y a eu un problème
+            $errors['global'] = "Une erreur s'est produite lors de l'édition de l'auteur.";
+        }
     }
 }
 ?>
@@ -51,6 +76,7 @@ if ($formulaire_soumis) {
     <?php include_once '../ressources/includes/head.php'; ?>
 
     <title>Editeur auteur - Administration</title>
+    <link rel="stylesheet" href="./administration/ressources/css/adm-form.css">
 </head>
 
 <body>
@@ -66,6 +92,11 @@ if ($formulaire_soumis) {
         <div class="mx-auto max-w-7xl py-6 px-4">
             <div class="py-6">
                 <?php if ($entite) { ?>
+                    <?php if (!empty($errors)): ?>
+                    <section class="banniere-erreur" role="alert" aria-live="polite">
+                        <p>Veuillez indiquer les champs requis.</p>
+                    </section>
+                <?php endif; ?>
                     <form method="POST" action="" class="rounded-lg bg-white p-4 shadow border-gray-300 border-1">
                         <section class="grid gap-6">
                             <input type="hidden" value="<?php echo $entite[
@@ -73,28 +104,34 @@ if ($formulaire_soumis) {
                             ]; ?>" name="id">
                             <div class="col-span-12">
                                 <label for="nom" class="block text-lg font-medium text-gray-700">Nom</label>
-                                <input type="text" value="<?php echo $entite[
-                                    'nom'
-                                ]; ?>"  name="nom" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" id="nom">
+                                <input type="text" value="<?= htmlspecialchars($formulaire_soumis ? $nom : $entite['nom']) ?>" name="nom" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" id="nom">   
+                                <?php if (!empty($errors['nom'])): ?>
+                                    <span class="text-red-600 mt-2"><?= $errors['nom'] ?></span>
+                                <?php endif; ?>
                             </div>
                             <div class="col-span-12">
                                 <label for="prenom" class="block text-lg font-medium text-gray-700">Prénom</label>
-                                <input type="text" value="<?php echo $entite[
-                                    'prenom'
-                                ]; ?>" name="prenom" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" id="prenom">
+                                <input type="text" value="<?= htmlspecialchars($formulaire_soumis ? $prenom : $entite['prenom']) ?>" name="prenom" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" id="prenom">
+                                <?php if (!empty($errors['prenom'])): ?>
+                                    <span class="text-red-600 mt-2"><?= $errors['prenom'] ?></span>
+                                <?php endif; ?>
                             </div>
                             <div class="col-span-12">
                                 <label for="avatar" class="block text-lg font-medium text-gray-700">Lien avatar</label>
-                                <input type="url" value="<?php echo $entite['lien_avatar']; ?>" name="lien_avatar" class="block w-full rounded-md border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-indigo-500" id="avatar">
+                                <input type="url" value="<?= htmlspecialchars($formulaire_soumis ? $lien_avatar : $entite['lien_avatar']) ?>" name="lien_avatar" class="block w-full rounded-md border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-indigo-500" id="avatar">
                                 <p class="text-sm text-gray-500">
                                     Mettre l'URL de l'avatar (chemin absolu)
                                 </p>
+                                <?php if (!empty($errors['lien_avatar'])): ?>
+                                    <span class="text-red-600 mt-2"><?= $errors['lien_avatar'] ?></span>
+                                <?php endif; ?>
                             </div>
                             <div class="col-span-12">
                                 <label for="lien_twitter" class="block text-lg font-medium text-gray-700">Lien twitter</label>
-                                <input type="url" value="<?php echo $entite[
-                                    'lien_twitter'
-                                ]; ?>" name="lien_twitter" class="block w-full rounded-md border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-indigo-500" id="lien_twitter">
+                                <input type="url" value="<?= htmlspecialchars($formulaire_soumis ? $lien_twitter : $entite['lien_twitter']) ?>" name="lien_twitter" class="block w-full rounded-md border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-indigo-500" id="lien_twitter">
+                                <?php if (!empty($errors['lien_twitter'])): ?>
+                                    <span class="text-red-600 mt-2"><?= $errors['lien_twitter'] ?></span>
+                                <?php endif; ?>
                             </div>
                             <div class="mb-3 col-md-6">
                                 <button type="submit" class="rounded-md bg-indigo-600 py-2 px-4 text-lg font-medium text-white shadow-sm hover:bg-indigo-700 focus-within:bg-indigo-700">Éditer</button>
@@ -102,7 +139,9 @@ if ($formulaire_soumis) {
                         </section>
                     </form>
                 <?php } else { ?>
-                    <!-- A compléter -->
+                    <section class="banniere-erreur" role="alert" aria-live="polite">
+                        <p>Auteur introuvable.</p>
+                    </section>
                 <?php } ?>
             </div>
         </div>
